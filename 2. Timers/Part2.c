4911a57b3d9a3cb6a3c18a0047fc747352692c51
Chris Stanski <stansk57@students.rowan.edu>
2023-02-25 14:45:09 -0500

    Timer Button switch

--- 2. Timers/Part2.c
+++ 2. Timers/Part2.c
@@ -2,12 +2,18 @@
  * Part2.c
  *
  *  Created on: Feb 11, 2023
- *      Author: Russell Trafford
+ *      Author: Chris Stanski
  *
  *      This code will need to change the speed of an LED between 3 different speeds by pressing a button.
  */
 
 #include <msp430.h>
+
+#define SLOW 50000
+#define MID 25000
+#define FAST 12500
+
+int state = FAST;
 
 void gpioInit();
 void timerInit();
@@ -16,8 +22,8 @@ void main(){
 
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
 
-    gpioInit();
     timerInit();
+    gpioInit();
 
     // Disable the GPIO power-on default high-impedance mode
     // to activate previously configured port settings
@@ -25,18 +31,31 @@ void main(){
 
     __bis_SR_register(LPM3_bits | GIE);
 
+
 }
 
 
 void gpioInit(){
-    // @TODO Initialize the Red or Green LED
 
-    // @TODO Initialize Button 2.3
+    P2OUT |= BIT3;
+    P2IES &= ~BIT3;     //
+    P2REN |= BIT3;
+    P2IE |= BIT3;
+
+    P6OUT &= ~BIT6;
+    P6DIR |= BIT6;      //
 
 
 }
 
 void timerInit(){
+
+       TB1CCTL0 = CCIE;                          // TBCCR0 interrupt enabled
+       TB1CCR0 = state;
+       TB1CTL = TBSSEL_1 | MC_2;                 // ACLK, continuous mode
+
+
+
     // @TODO Initialize Timer B1 in Continuous Mode using ACLK as the source CLK with Interrupts turned on
 
 }
@@ -50,7 +69,28 @@ void timerInit(){
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {
+
+    P2IFG &= ~BIT3;
+
+
+    switch (state){
+        case (SLOW):state=MID;
+        break;
+
+        case(MID):state=FAST;
+        break;
+
+        default:
+        case(FAST):state=SLOW;
+        break;
+
+    }
+
+
+
+
     // @TODO Remember that when you service the GPIO Interrupt, you need to set the interrupt flag to 0.
+
 
     // @TODO When the button is pressed, you can change what the CCR0 Register is for the Timer. You will need to track what speed you should be flashing at.
 
@@ -61,7 +101,9 @@ __interrupt void Port_2(void)
 #pragma vector = TIMER1_B0_VECTOR
 __interrupt void Timer1_B0_ISR(void)
 {
+
+    P6OUT ^= BIT6;
+    TB1CCR0 += state;
+
     // @TODO You can toggle the LED Pin in this routine and if adjust your count in CCR0.
 }
-
-
